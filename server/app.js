942ca710a511011765b4ddda350c0e9f96800f6f
Sham Siva <ssiva76390@gmail.com>
2026-09-05 10:28:11 +0530

    Phase 1 - Server Running

---
+++ server/app.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express = require("express");
+const db = require("./config/db");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("🚀 DigiTrace Server Running Successfully");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+});
